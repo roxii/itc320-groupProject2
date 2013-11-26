2fbd580c49aa6a63344f263a52c980d419cb9465
Jacob <dubsjw@gmail.com>
2013-11-26 17:50:20 -0500

    Changed the files to fresh copy for server submission.

--- chess.js
+++ chess.js
@@ -7,15 +7,6 @@ var no = 0
 var yes = 1
 var playerCheck = no
 
-//Create game board step1 
-var game = new Array(8);
-for (var i = 0; i < 8; i++) {
-game[i] = new Array(8);
-for (var j = 0; j < 8; j++) {
-	game[i][j] = 'R';
-}
-}
-
 // 2-D Array Chess game starting state
 var game1 =
 [['R','N','B','Q','K','B','N','R'],
@@ -27,16 +18,6 @@ var game1 =
 ['p','p','p','p','p','p','p','p'],
 ['r','n','b','q','k','b','n','r']];
 
-//print(game.join('\n') + '\n\n');
-
-//print(game1.join('\n') + '\n\n');
-
-// An example move
-//Move queen's bishop's pawn forward one
-//game1[5][2] = game1[6][2];
-//game1[6][2] = ' ';
-
-//print(game1.join('\n'));
 
 function isValidMove(x1, y1, x2, y2) {
 	var movingFrom = game1[x1][y1];
